4ebe419c868d5d3bbcf135824765948edc65add5
Matoz <56786508+Matozz@users.noreply.github.com>
2021-04-10 22:51:03 +0800

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,11 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, Matoz Here<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle" style={{marginRight:8}}>@matozz</span>
+          <span className="handle">@matozmusic</span>
+          <h2>Coding & Music & Life.</h2>
           <SocialList />
         </div>
       </div>
